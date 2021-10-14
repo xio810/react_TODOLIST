d76e948450c1ec0a32ea89b120bb380e81fc0e1a
xio810 <boi4806@naver.com>
2021-10-14 21:37:22 +0900

    Recoil 도입

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,53 @@
 import { useState } from "react";
 import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
 
 const App = () => {
   return (
-    <Router>
-      <Head />
-      <Switch>
-        <Route path="/page2">
-          <SecondPage />
-        </Route>
-        <Route path="/page1">
-          <FirstPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
+    <RecoilRoot>
+      <RecoilApp />
+    </RecoilRoot>
   );
 };
 
-const Head = () => {
-  const pagesPaths = [
-    ["/", "메인"],
-    ["/page1", "페이지1"],
-    ["/page2", "페이지2"],
-  ];
+const RecoilApp = () => {
+  const [subNo, setSubNo] = useState(1);
 
   return (
-    <nav>
-      <ul>
-        {pagesPaths.map(([pagePath, pageName]) => (
-          <li>
-            <Link to={pagePath}>{pageName}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <button onClick={() => setSubNo(subNo == 1 ? 2 : 1)}>페이지 전환</button>
+      {subNo == 1 ? <Sub1 /> : <Sub2 />}
+    </>
   );
 };
 
-const SecondPage = () => {
-  return <h1>두번째 페이지</h1>;
+const Sub1 = () => {
+  const [sub1Count, setSub1Count] = useState(0);
+
+  return (
+    <>
+      <hr />
+      <div>서브1</div>
+      <button onClick={() => setSub1Count(sub1Count + 1)}>{sub1Count}</button>
+    </>
+  );
 };
 
-const FirstPage = () => {
-  return <h1>첫 페이지</h1>;
-};
+const Sub2 = () => {
+  const [sub2Count, setSub2Count] = useState(0);
 
-const HomePage = () => {
-  return <h1>홈 페이지</h1>;
+  return (
+    <>
+      <hr />
+      <div>서브2</div>
+      <button onClick={() => setSub2Count(sub2Count + 1)}>{sub2Count}</button>
+    </>
+  );
 };
 export default App;
