f80b175e3eebbfffcfbd12a93e8ef2daa02b6879
xio810 <boi4806@naver.com>
2021-10-15 20:36:06 +0900

    Recoil Atom 도입

--- src/App.jsx
+++ src/App.jsx
@@ -17,35 +17,49 @@ const App = () => {
 };
 
 const RecoilApp = () => {
+  const [sub1Count] = useRecoilState(sub1CountAtom);
+  const sub2Count = useRecoilValue(sub2CountAtom);
+
   const [subNo, setSubNo] = useState(1);
 
   return (
     <>
-      <button onClick={() => setSubNo(subNo == 1 ? 2 : 1)}>페이지 전환</button>
+      <div>sub1 count : {sub1Count}</div>
+      <div>sub2 count : {sub2Count}</div>
+
+      <button onClick={() => setSubNo(subNo == 1 ? 2 : 1)}>change</button>
       {subNo == 1 ? <Sub1 /> : <Sub2 />}
     </>
   );
 };
 
-const Sub1 = () => {
-  const [sub1Count, setSub1Count] = useState(0);
+const sub1CountAtom = atom({
+  key: "app/sub1Count",
+  default: 0,
+});
 
+const Sub1 = () => {
+  const [sub1Count, setSub1Count] = useRecoilState(sub1CountAtom);
   return (
     <>
       <hr />
-      <div>서브1</div>
+      <div>sub1</div>
       <button onClick={() => setSub1Count(sub1Count + 1)}>{sub1Count}</button>
     </>
   );
 };
 
-const Sub2 = () => {
-  const [sub2Count, setSub2Count] = useState(0);
+const sub2CountAtom = atom({
+  key: "app/sub2Count",
+  default: 0,
+});
 
+const Sub2 = () => {
+  const [sub2Count, setSub2Count] = useRecoilState(sub2CountAtom);
   return (
     <>
       <hr />
-      <div>서브2</div>
+      <div>sub2</div>
       <button onClick={() => setSub2Count(sub2Count + 1)}>{sub2Count}</button>
     </>
   );
