f66c19842550f1eeccb8bdeb737143a38da0dc4d
xio810 <boi4806@naver.com>
2021-10-19 21:24:32 +0900

    poké api list & font

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
 const queryClient = new QueryClient();
 
-export default function App() {
+const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Example />
+      <Main />
     </QueryClientProvider>
   );
-}
+};
 
-function Example() {
-  const { isLoading, error, data } = useQuery("repoData", () =>
-    fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
-      (res) => res.json()
-    )
+const Main = () => {
+  const { isLoading, error, data } = useQuery("pokemon", () =>
+    fetch("https://pokeapi.co/api/v2/pokemon").then((res) => res.json())
   );
 
   if (isLoading) return "Loading...";
@@ -23,12 +21,14 @@ function Example() {
   if (error) return "An error has occurred: " + error.message;
 
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <strong>👀 {data.subscribers_count}</strong>{" "}
-      <strong>✨ {data.stargazers_count}</strong>{" "}
-      <strong>🍴 {data.forks_count}</strong>
-    </div>
+    <>
+      <ul>
+        {data.results.map((poke) => (
+          <li key={poke}>{poke.name}</li>
+        ))}
+      </ul>
+    </>
   );
-}
+};
+
+export default App;
