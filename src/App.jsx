97cd6c55a0df8ea86ab8dc7692bca535bf275aee
xio810 <boi4806@naver.com>
2021-10-21 20:32:35 +0900

    css affect

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Main />
+      <PokeListPage />
     </QueryClientProvider>
   );
 };
 
-const Main = () => {
-  const { isLoading, error, data } = useQuery("pokemon", () =>
+const PokeListPage = () => {
+  const { isLoading, error, data } = useQuery("pokeList", () =>
     fetch("https://pokeapi.co/api/v2/pokemon").then((res) => res.json())
   );
 
@@ -20,20 +20,33 @@ const Main = () => {
 
   if (error) return "An error has occurred: " + error.message;
 
+  const getNoFromUrl = (url) => {
+    const urlBits = url.split("/");
+    return parseInt(urlBits[urlBits.length - 2]);
+  };
+
   return (
     <>
-      <h1>POKÉMON</h1>
+      <h1 className="py-2 text-center font-bold bg-gray-500 text-white sticky top-0">
+        포켓몬 리스트
+      </h1>
       <ul>
         {data.results.map((poke) => {
-          const urlBits = poke.url.split("/");
-          const no = parseInt(urlBits[urlBits.length - 2]);
+          const no = getNoFromUrl(poke.url);
 
           return (
-            <li key={poke}>
+            <li key={poke} className="flex items-center">
               <img
                 src={`https://cdn.jsdelivr.net/gh/PokeAPI/sprites/sprites/pokemon/${no}.png`}
               />
-              <h2>{poke.name}</h2>
+              <div className="ml-2">
+                <h2 className="w-20">
+                  <span className="badge badge-outline badge-primary">
+                    {no}
+                  </span>
+                </h2>
+                <h2>{poke.name}</h2>
+              </div>
             </li>
           );
         })}
